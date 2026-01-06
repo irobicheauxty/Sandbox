cc461e6ff13fd7ee7e84d9baae732d26bcb8fa41
Ty Robicheaux <irobicheauxty@gmail.com>
2026-01-06 14:11:54 -0500

    Added variable with User Input

--- Sandbox.py
+++ Sandbox.py
@@ -9,3 +9,11 @@ name = "Ty Robicheaux"
 age = 15
 
 print("My name is",name,"it is the year",year,"and I am",age,"years old!")
+
+print("")
+
+year = input("What year is it: ")
+name = input("What is your name: ")
+age = input("How old are you: ")
+
+print("\nWelcome",name,"I see you are",age,"years old in the year of",year + ".")
